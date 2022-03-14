0b51cd35d719ae8f54231b8f1f4432c81760838c
JP Zhang <zjplab@gmail.com>
2022-03-14 18:04:33 +0800

    Update printer.py

--- printer.py
+++ printer.py
@@ -1,6 +1,6 @@
 def dynamic_display_dict(input_dict:dict)->None:
     """Dynamically print a dictionary and refresh it
-
+    Inspired by https://stackoverflow.com/questions/39455022/python-3-print-update-on-multiple-lines
     Parameters
     ----------
     input_dict : dict
