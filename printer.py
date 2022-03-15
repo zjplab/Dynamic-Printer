e79b318f9ed3e6d58fa03b0eb2c76b504a5ef91e
JP Zhang <zjplab@gmail.com>
2022-03-15 15:07:41 +0800

    Update printer.py

--- printer.py
+++ printer.py
@@ -7,7 +7,7 @@ def dynamic_display_dict(input_dict:dict)->None:
         The dict you want to print, every element will be printed
     """    
     size=len(input_dict)
-    UP = "\x1B["+str(size)+"A"
+    UP = "\x1B["+str(size+1)+"A"
     CLR = "\x1B[0K"
     res=f"{UP}"
     for k,v in input_dict.items():
